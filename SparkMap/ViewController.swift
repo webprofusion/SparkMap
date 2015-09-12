7b7dee280eea12f27c911586cb9683ac213d7613
Christopher Cook <christopher.cook@webprofusion.com>
2015-09-12 18:47:35 +0800

    Initial Commit

---
+++ SparkMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SparkMap
+//
+//  Created by Christopher Cook on 12/09/2015.
+//  Copyright © 2015 Christopher Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
