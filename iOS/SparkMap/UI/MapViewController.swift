ab9f76f821145240671271040037e921d136bbc4
Christopher Cook <christopher.cook@webprofusion.com>
2015-09-16 21:43:39 +0800

    Fix build after file move

--- iOS/SparkMap/UI/MapViewController.swift
+++ iOS/SparkMap/UI/MapViewController.swift
@@ -61,4 +61,28 @@ class MapViewController: UIViewController {
     }
     
 }
+/*
+extension MapViewController: MKMapViewDelegate {
+    
+    // 1
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        if let annotation = annotation as? POIMarker {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+                as? MKPinAnnotationView { // 2
+                    dequeuedView.annotation = annotation
+                    view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton.buttonWithType(.DetailDisclosure) as! UIView
+            }
+            return view
+        }
+        return nil
+    }
+}*/
 
